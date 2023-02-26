1da6f2803bffb01a33b9db2fd4a7ffcf804906b9
Aref <Aref.Najmaddini@gmail.com>
2023-02-26 17:39:00 +0330

    Writing menu and abstractions

--- src/Menu.java
+++ src/Menu.java
@@ -14,21 +14,20 @@ public class Menu {
                 --->\040""");
         int key = Scan.nextInt();
 
-        switch (key){
-            case 1: // PvP menu
-                this.isPvP_menu();
-                break;
-            case 2: // Info menu
+        switch (key) {
+            case 1 -> // PvP menu
+                    this.isPvP_menu();
+            case 2 -> { // Info menu
                 this.info();
                 this.main_menu();
-                break;
-            case 3: // Exit
-                System.out.println("Tnx for your attention ^.^\n\n");
-                break;
-            default: // if input was not valid
+            }
+            case 3 -> // Exit
+                    System.out.println("Tnx for your attention ^.^\n\n");
+            default -> { // if input was not valid
                 System.out.println("Please Enter the Correct Entry (*_ *;) \n\n");
                 this.main_menu();
                 System.out.println("writing menu and abstractions");
+            }
         }
 
 
