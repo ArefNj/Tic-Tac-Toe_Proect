8b4277f779374f479ad691e762a8dd62dfa4d396
Aref <Aref.Najmaddini@gmail.com>
2023-02-28 09:08:42 +0330

    add info

--- src/App.java
+++ src/App.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.io.IOException;
 
 public class App {
 
     private boolean isPvP;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
     Menu main = new Menu();
     main.main_menu();
 
